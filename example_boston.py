fff44db2d3a6289b4e773a652e51d39f6d768bff
chriswbartley <chris@empirikalsystems.com>
2017-09-01 14:03:27 +0800

    Updated boston example to show current usage

--- example_boston.py
+++ example_boston.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor,GradientBoostingClassifier
 from rulefit import RuleFit
 
 
@@ -12,11 +12,40 @@ X = boston_data.drop("medv", axis=1)
 features = X.columns
 X = X.as_matrix()
 
-gb = GradientBoostingRegressor(n_estimators=100, max_depth=3, learning_rate=0.01)
-rf = RuleFit(gb)
+typ='classifier'
+use_mt_feats=True
 
-rf.fit(X, y, feature_names=features)
+if use_mt_feats:
+    incr_feats=[6,9]
+    decr_feats=[1,8,12,13]
+else:
+    incr_feats=None
+    decr_feats=None
+        
+if typ=='regressor':
 
+    rf = RuleFit(tree_size=4,sample_fract='default',max_rules=2000,
+             memory_par=0.01,
+             tree_generator=None,n_feats=X.shape[1],incr_feats=incr_feats,decr_feats=decr_feats,
+            rfmode='regress',lin_trim_quantile=0.025,
+            lin_standardise=True, exp_rand_tree_size=True,random_state=1) 
+    rf.fit(X, y, feature_names=features)
+elif typ=='classifier':
+    y_class=y.copy()
+    y_class[y_class<21]=-1
+    y_class[y_class>=21]=+1
+    N=X.shape[0]
+    rf = RuleFit(tree_size=4,sample_fract='default',max_rules=2000,
+                 memory_par=0.01,
+                 tree_generator=None,n_feats=X.shape[1],incr_feats=incr_feats,decr_feats=decr_feats,
+                rfmode='classify',lin_trim_quantile=0.025,
+                lin_standardise=True, exp_rand_tree_size=True,random_state=1) 
+    rf.fit(X, y_class, feature_names=features)
+    
+res1=rf.predict(X)
 rules = rf.get_rules()
 
-rules = rules[rules.coef != 0].sort("support")
+rules = rules[rules.coef != 0].sort_values(by="support")
+num_rules_rule=len(rules[rules.type=='rule'])
+num_rules_linear=len(rules[rules.type=='linear'])
+
