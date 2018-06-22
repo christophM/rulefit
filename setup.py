2a66e1cf901ea3b32a47140bb907ddb9dfedd5d0
Christopher Bartley <christopher.bartley@research.uwa.edu.au>
2018-06-22 20:14:47 +0800

    Update version to 0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='RuleFit',
-      version='0.1',
+      version='0.3',
       description='RuleFit algorithm',
       author='Christoph Molnar',
       author_email='christoph.molnar@gmail.com',
