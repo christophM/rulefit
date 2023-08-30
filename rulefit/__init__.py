4a1586bdf6a8a37ce05760d7e32c0fe648601f67
jak_tkvs <32125829+jckkvs@users.noreply.github.com>
2023-08-30 10:59:23 +0900

    Add RuleFitRegressor   RuleFitClassifier to __init__.py
    Follow up https://github.com/christophM/rulefit/pull/51

--- rulefit/__init__.py
+++ rulefit/__init__.py
@@ -1,3 +1,12 @@
-from .rulefit import RuleCondition, Rule, RuleEnsemble, RuleFit, FriedScale, Winsorizer
+from .rulefit import (
+    RuleCondition,
+    Rule,
+    RuleEnsemble,
+    RuleFit,
+    FriedScale,
+    Winsorizer,
+    RuleFitRegressor,
+    RuleFitClassifier,
+)
 
 __all__ = ["rulefit"]
